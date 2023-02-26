ddeef5491ece18c518eb30f38d827e2f4882479d
chitsimran <chitsimranwaraich0831@gmail.com>
2023-02-26 23:27:10 +0530

    Add Design pattern interface

---
+++ design-patterns/src/main/java/com/singh/design/patterns/DesignPattern.java
@@ -0,0 +1,9 @@
+package com.singh.design.patterns;
+
+import com.singh.design.patterns.enums.DesignPatternName;
+
+public interface DesignPattern {
+    void run();
+
+    DesignPatternName getName();
+}
