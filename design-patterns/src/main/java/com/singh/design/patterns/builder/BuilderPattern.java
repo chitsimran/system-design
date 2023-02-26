f09db53e91d309a2bd8323f1326e4e2b140a4f45
chitsimran <chitsimranwaraich0831@gmail.com>
2023-02-26 23:27:37 +0530

    Add Builder pattern

---
+++ design-patterns/src/main/java/com/singh/design/patterns/builder/BuilderPattern.java
@@ -0,0 +1,24 @@
+package com.singh.design.patterns.builder;
+
+import com.singh.design.patterns.DesignPattern;
+import com.singh.design.patterns.enums.DesignPatternName;
+
+public class BuilderPattern implements DesignPattern {
+    @Override
+    public void run() {
+        System.out.println("Creating a Player with Builder pattern.");
+        Player player1 = new Player.PlayerBuilder()
+                .name("Singh")
+                .country("India")
+                .debutYear(2023)
+                .matchesPlayed(8)
+                .build();
+
+        System.out.println(player1.toString());
+    }
+
+    @Override
+    public DesignPatternName getName() {
+        return DesignPatternName.BUILDER;
+    }
+}
