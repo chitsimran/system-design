01c28394b5a413a5bbaf8d8dd2852f8f83008eed
chitsimran <chitsimranwaraich0831@gmail.com>
2023-02-26 23:28:11 +0530

    Add Main class for app

---
+++ design-patterns/src/main/java/com/singh/design/patterns/Main.java
@@ -0,0 +1,15 @@
+package com.singh.design.patterns;
+
+import com.singh.design.patterns.enums.DesignPatternName;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Application running!");
+
+        DesignPatternFactory designPatternFactory = DesignPatternFactory.getInstance();
+
+        DesignPattern designPattern = designPatternFactory.getDesignPattern(DesignPatternName.BUILDER);
+        System.out.println("Running code for: " + designPattern.getName());
+        designPattern.run();
+    }
+}
