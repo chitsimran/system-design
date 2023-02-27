505cb50ae8accdef0fc4542b179d8487c1083672
chitsimran <chitsimranwaraich0831@gmail.com>
2023-02-27 20:40:47 +0530

    Add javadoc

--- design-patterns/src/main/java/com/singh/design/patterns/adapter/StudentAdapter.java
+++ design-patterns/src/main/java/com/singh/design/patterns/adapter/StudentAdapter.java
@@ -1,6 +1,12 @@
 package com.singh.design.patterns.adapter;
 
 public class StudentAdapter {
+    /**
+     * Converts a SchoolStudent to a CollegeStudent
+     *
+     * @param schoolStudent Object of SchoolStudent
+     * @return CollegeStudent with details of passed SchoolStudent
+     */
     public static CollegeStudent asCollegeStudent(SchoolStudent schoolStudent) {
         return new CollegeStudent(schoolStudent.getFirstName() + " " + schoolStudent.getLastName());
     }
