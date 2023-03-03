f6c127e5ab4eb8ce37d6112e45212a694ceca959
chitsimran <chitsimranwaraich0831@gmail.com>
2023-03-03 21:27:13 +0530

    Add base class

---
+++ design-patterns/src/main/java/com/singh/design/patterns/decorator/pizza/MargheritaPizza.java
@@ -0,0 +1,8 @@
+package com.singh.design.patterns.decorator.pizza;
+
+public class MargheritaPizza extends Pizza {
+    @Override
+    public Integer cost() {
+        return 140;
+    }
+}
---
+++ design-patterns/src/main/java/com/singh/design/patterns/decorator/pizza/PepperoniPizza.java
@@ -0,0 +1,8 @@
+package com.singh.design.patterns.decorator.pizza;
+
+public class PepperoniPizza extends Pizza {
+    @Override
+    public Integer cost() {
+        return 200;
+    }
+}
